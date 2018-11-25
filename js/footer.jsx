a1cdeedf1f6740856ec4aafe7a1010f9d1c00a00
maciej <maciej.ostrows@gmail.com>
2018-11-25 14:13:29 +0100

    ikony w footer

--- js/footer.jsx
+++ js/footer.jsx
@@ -7,6 +7,11 @@ export default class Footer extends React.Component{
             <div className={'footer'}>
                 <div>Football data provided by the Football-Data.org API</div>
                 <div>Created by Maciej Ostrowski</div>
+                <div>
+                    <a href="mailto:maciej.ostrows@gmail.com"><img src="./icons/gmail.png"></img></a>
+                    <a href="https://www.linkedin.com/in/maciej-ostrowski" target="_blank"><img src="./icons/linkedin.png"></img></a>
+                    <a href="https://github.com/maciejostrows" target="blank"><img src="./icons/github.png"></img></a>
+                </div>
             </div>
         )
     }
